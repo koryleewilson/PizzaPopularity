e53cfeb28bcd0a09e4dddfe5a373c28943620200
koryleewilson <32466365+koryleewilson@users.noreply.github.com>
2018-04-06 14:12:45 -0600

    finish example

---
+++ PizzaPopularity/Models/PizzaChoice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace PizzaPopularity.Models
+{
+    /// <summary>
+    /// Pizza topping choice
+    /// </summary>
+    public class PizzaChoice : IEquatable<PizzaChoice>
+    {
+        /// <summary>
+        /// toppings array
+        /// </summary>
+        public string[] toppings;
+
+        /// <summary>
+        /// equality of toppings type
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>true if equal - false if not</returns>
+        public bool Equals(PizzaChoice other)
+        {
+            bool ret = false;
+
+            // check the count
+            if (this.toppings.Count() == other.toppings.Count())
+            {
+                // see if the array contains the same toppings
+                if (this.toppings.ToList().OrderBy(b => b).SequenceEqual(other.toppings.ToList().OrderBy(o => o)))
+                {
+                    ret = true;
+                }
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// override for IEquatable
+        /// </summary>
+        /// <param name="obj">The object to compare</param>
+        /// <returns>true if equal - false if not</returns>
+        public override bool Equals(object obj)
+        {
+            // Call the implementation from IEquatable
+            return Equals((PizzaChoice)obj);
+        }
+
+        /// <summary>
+        /// get the hash code for PizzaChoice object
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            int ret = 0;
+            this.toppings.ToList().OrderBy(t => t).ToList().ForEach(o => ret += o.GetHashCode());
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Return the object how we want it to look
+        /// </summary>
+        /// <returns>string representation</returns>
+        public override string ToString()
+        {
+            // String representation.
+            string sRet = "\r\n";
+            this.toppings.ToList().ForEach(b => sRet += "\t" + b.ToString() + "\r\n");
+            return sRet;
+        }
+    }
+}
--- PizzaPopularity/Program.cs
+++ PizzaPopularity/Program.cs
@@ -1,15 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System;
+using PizzaPopularity.Models;
 
 namespace PizzaPopularity
 {
-    class Program
+    /// <summary>
+    /// Test class for getting top 20 most popular bikes
+    /// </summary>
+    public class Program
     {
+        /// <summary>
+        /// Our input Json is here
+        /// </summary>
+        private const string POPULARITYRESPONSES = @"http://files.olo.com/pizzas.json";
+
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">input is ignored</param>
         static void Main(string[] args)
         {
+            // read in the data - it is json
+            string popularityData = ReadInPopularityData();
+
+            //deserialize the Json
+            PizzaChoice[] toppingsList = JsonConvert.DeserializeObject<PizzaChoice[]>(popularityData);
+
+            //Get the 20 most popular combinations
+            List<string> popularPizzas = GetMostPopular(toppingsList, 20);
+
+            int rank = 1;
+            List<string> output = new List<string>();
+
+            popularPizzas.ToList().ForEach(p => output.Add(p += $"\r\n rank {rank++}\r\n"));
+
+            // output the results
+            FileStream fs = File.Create("TopTwenty.txt");
+            StreamWriter sw = new StreamWriter(fs);
+            output.ForEach(pb => sw.WriteLine(pb));
+
+            sw.Flush();
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Inspect the array and return the most repeated
+        /// </summary>
+        /// <param name="pizzaChoice">The array to inspect</param>
+        /// <param name="count">count to return</param>
+        /// <returns>The most repeated Purchased objects</returns>
+        public static List<string> GetMostPopular(PizzaChoice[] pizzaChoice, int count)
+        {
+            //creat a dictionary container
+            Dictionary<PizzaChoice, int> purchCount = new Dictionary<PizzaChoice, int>();
+            foreach (PizzaChoice purch in pizzaChoice)
+            {
+                // add the count of each topping combination
+                if (!purchCount.ContainsKey(purch))
+                {
+                    purchCount.Add(purch, 1);
+                }
+                else
+                {
+                    purchCount[purch]++;
+                }
+            }
+
+            // compile the results
+            return purchCount?.OrderByDescending(pc => pc.Value).Select(v => $"Toppings:{v.Key} ordered {v.Value} number of times").Take(count).ToList();
+        }
+
+        /// <summary>
+        /// Read in the popularity data
+        /// </summary>
+        /// <returns>Json data</returns>
+        private static string ReadInPopularityData()
+        {
+            HttpWebRequest request = (HttpWebRequest)
+
+            // get the data url
+            WebRequest.Create(POPULARITYRESPONSES);
+
+            // execute the request
+            HttpWebResponse response = (HttpWebResponse)
+                request.GetResponse();
+
+            // create a buffer
+            int size = 1000;
+            byte[] buf = new byte[size];
+
+            // create our string builder
+            StringBuilder sb = new StringBuilder();
+
+            // we will read data via the response stream
+            Stream resStream = response.GetResponseStream();
+            string tempString = null;
+            int count = 0;
+
+            do
+            {
+                // fill the buffer with data
+                count = resStream.Read(buf, 0, buf.Length);
+
+                // make sure we read some data
+                if (count != 0)
+                {
+                    // translate from bytes to ASCII text
+                    tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                    // continue building the string
+                    sb.Append(tempString);
+                }
+            }
+            while (count > 0); // any more data to read?
+
+            // return the data
+            return sb.ToString();
         }
     }
 }
